a7844443ae0dc2914832642f967480a557fe6c6e
IsaiasRO <Isaiasilvestre@gmail.com>
2017-09-15 14:55:54 -0500

    controller
    corregi movimiento

--- Assets/Scrips/controller.cs
+++ Assets/Scrips/controller.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class controller: MonoBehaviour {
-	[SerializeField] private Transform leftPaddle;
-	[SerializeField] private Transform rightPaddle;
+	[SerializeField]  Transform leftPaddle;
+	[SerializeField]  Transform rightPaddle;
 	[SerializeField] int speed = 4;
 
 	// Use this for initialization
@@ -14,19 +14,19 @@ public class controller: MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.W)) {
+		if (Input.GetKey (KeyCode.W)&& leftPaddle.position.y<3.7) {
 			//move paddle 1
 			leftPaddle.Translate(Vector2.up * Time.deltaTime * speed);
 		}
-		if (Input.GetKey (KeyCode.S)) {
+		if (Input.GetKey (KeyCode.S)&& leftPaddle.position.y>-3.7) {
 			//move paddle 1
 			leftPaddle.Translate(Vector2.down * Time.deltaTime * speed);
 		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
+		if (Input.GetKey (KeyCode.UpArrow)&& rightPaddle.position.y<3.7) {
 			//move paddle 2
 			rightPaddle.Translate(Vector2.up * Time.deltaTime * speed);
 		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
+		if (Input.GetKey (KeyCode.DownArrow)&& rightPaddle.position.y>-3.7) {
 			//move paddle 2
 			rightPaddle.Translate(Vector2.down * Time.deltaTime * speed);
 		}
