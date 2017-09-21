0c72e8e939e1aa1c58a7897a4fb3a3014118e675
IsaiasRO <Isaiasilvestre@gmail.com>
2017-09-21 12:03:51 -0500

    contador
    juego terminado

--- Assets/sprits/Scrips/UIManager.cs
+++ Assets/sprits/Scrips/UIManager.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
-
+	[SerializeField] Text Setcounter1;
+	[SerializeField] Text Setcounter2;
+	int counter = 0;
+	int counter2 = 0;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	public void UpdateCounter1 () {
+		counter += 1;
+		Setcounter1.text = "" +counter; 
+	}
+	public void UpdateCounter2 () {
+		counter2 += 1;
+		Setcounter2.text = "" + counter2; 
 	}
 }
--- Assets/sprits/Scrips/ball.cs
+++ Assets/sprits/Scrips/ball.cs
@@ -2,18 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ball : MonoBehaviour {
-	private Rigidbody2D rb;
+public class ball: MonoBehaviour {
 
-	// Use this for initialization
+	Rigidbody2D rb;
+	[SerializeField] UIManager uimanager;
 	void Start (){
-		
+
 		rb = GetComponent<Rigidbody2D> ();
-		rb.velocity = new Vector2 (10f,10f);
+		StartCoroutine (Pause ());
 
 	}
 	void Update(){
 		
+		if (transform.position.x < -10.8f) {
+			transform.position = Vector2.zero;
+			rb.velocity = Vector2.zero;
+			uimanager.UpdateCounter2();
+			StartCoroutine (Pause ());
+		}
+		//ball goes too far right
+		if (transform.position.x > 10.8f) {
+			transform.position = Vector2.zero;
+			rb.velocity = Vector2.zero;
+			uimanager.UpdateCounter1();
+			StartCoroutine (Pause ());
+		}
+	}
+	IEnumerator Pause () {
+		
+		yield return new WaitForSeconds (2f);
+		RandownBall ();
 	}
 
+
+	void RandownBall(){
+
+		transform.position = Vector2.zero;
+		//velocity in ball randown
+		rb.velocity = new Vector2 (8f, 0f);
+		//Random Direction
+		int xDirection = Random.Range (0, 2);
+		int yDirection = Random.Range (0, 3);
+
+		Vector2 launchDirection = new Vector2 ();
+
+		if (xDirection == 0) {
+			launchDirection.x = -8f;
+		}
+		if (xDirection == 1) {
+			launchDirection.x = 8f;
+		}
+		if (yDirection == 0) {
+			launchDirection.y = -8f;
+		}
+		if (yDirection == 1) {
+			launchDirection.y = 8f;
+		}
+		if (yDirection == 2) {
+			launchDirection.y = 0;
+		}
+		//asigna velocidad 
+		rb.velocity = launchDirection;
+
+	}
 }
